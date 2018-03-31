f9e3de01873d45924ec5e96a03142d704f20286b
Kaustubh Damania <kaustubh.damania@gmail.com>
2018-03-31 19:16:04 +0530

    Initial commit

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+	public void changeToScene(int changeTheScene){
+		SceneManager.LoadScene (changeTheScene);
+	}
+}
